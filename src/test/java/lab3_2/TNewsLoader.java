e61e3e6742cf084e1af48d63ddb1bcea03e321d5
195024 <195024@edu.p.lodz.pl>
2017-04-04 12:52:12 +0200

    created @Before and a test to check if news lists sizes are correct

---
+++ src/test/java/lab3_2/TNewsLoader.java
@@ -0,0 +1,67 @@
+package lab3_2;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import edu.iis.mto.staticmock.Configuration;
+import edu.iis.mto.staticmock.ConfigurationLoader;
+import edu.iis.mto.staticmock.IncomingInfo;
+import edu.iis.mto.staticmock.IncomingNews;
+import edu.iis.mto.staticmock.NewsLoader;
+import edu.iis.mto.staticmock.NewsReaderFactory;
+import edu.iis.mto.staticmock.PublishableNews;
+import edu.iis.mto.staticmock.SubsciptionType;
+import edu.iis.mto.staticmock.reader.FileNewsReader;
+import edu.iis.mto.staticmock.reader.NewsReader;
+
+import static org.powermock.api.mockito.PowerMockito.*;
+import org.mockito.internal.util.reflection.*;
+
+import static org.hamcrest.CoreMatchers.*;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({ ConfigurationLoader.class, NewsReaderFactory.class, PublishableNews.class })
+
+public class TNewsLoader {
+
+	private ConfigurationLoader configLoader = null;
+
+	@Before
+	public void setup() {
+		configLoader = PowerMockito.mock(ConfigurationLoader.class);
+		PowerMockito.mockStatic(ConfigurationLoader.class);
+
+		Configuration config = new Configuration();
+		NewsReader newsReader = PowerMockito.mock(FileNewsReader.class);
+		PowerMockito.mockStatic(NewsReaderFactory.class);
+		IncomingNews incomingNews = new IncomingNews();
+
+		Whitebox.setInternalState(config, "readerType", "tReader");
+
+		incomingNews.add(new IncomingInfo("pub1", SubsciptionType.NONE));
+		incomingNews.add(new IncomingInfo("pub2", SubsciptionType.NONE));
+		incomingNews.add(new IncomingInfo("sub1", SubsciptionType.A));
+		incomingNews.add(new IncomingInfo("sub2", SubsciptionType.B));
+
+		PowerMockito.when(ConfigurationLoader.getInstance()).thenReturn(configLoader);
+		when(configLoader.loadConfiguration()).thenReturn(config);
+		when(newsReader.read()).thenReturn(incomingNews);
+		when(NewsReaderFactory.getReader("tReader")).thenReturn(newsReader);
+	}
+
+	@Test
+	public void checkIfNewsSizesAreCorrect() {
+		NewsLoader newsLoader = new NewsLoader();
+		PublishableNews publishableNews = newsLoader.loadNews();
+
+		assertThat(publishableNews.getPublicContent().size(), is(equalTo(2)));
+		assertThat(publishableNews.getSubscribentContent().size(), is(equalTo(2)));
+	}
+
+}
